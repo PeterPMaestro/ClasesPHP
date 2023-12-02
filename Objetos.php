2d9d405c3059e66370824175eb98cf2d93a3869a
Pedro <53993595+PeterPMaestro@users.noreply.github.com>
2023-12-02 11:44:07 +0100

    Add files via upload

--- Objetos.php
+++ Objetos.php
@@ -3,12 +3,13 @@
 include "cliente.php";
 include "ListaClientes.php";
 
-
+ //$u1 = new Usuario("Juan", "LKHKL3"); // Probando a añadir un usuario antes de hacer la clase abstracta.
  $c1 = new cliente("Pepe", "JAJA03", "600606060", "standard");
  $c2 = new cliente("Lola", "YAYAYA01", "655555545", "premium");
 
  $c1 -> Mostrar();
- $c2 -> Mostrar();
+
+
  $lc = new ListaClientes();
  $lc -> AddUser($c1);
  $lc -> AddUser($c2);
--- cliente.php
+++ cliente.php
@@ -1,56 +1,43 @@
 <?php
 
+
+//HERENCIA E IMPLEMENTACIÓN DE UNA INTERFAZ
 include "Usuario.php";
 include "interfazCliente.php";
 
 
-class cliente extends Usuario implements iCliente{
-    private $nombre;
-    private $password;
+class cliente extends Usuario implements iCliente{  
     private $telefono;
     private $contrato;
 
 public function __construct($nombre, $password, $telefono, $contrato){
-    $this -> setNombre($nombre);
-    $this -> setPassword($password);
+    parent::__construct($nombre, $password); 
     $this -> telefono = $telefono;
     $this -> contrato = $contrato;
-}
-public function getNombre(){
-    return $this -> nombre;
-}
-public function setNombre ($nombre){
-    $this -> nombre = $nombre;
-}
-public function getPassword (){
-    return $this -> password;
-}
-public function setPassword ($password){
-    $this -> password = $password;
+
 }
 public function setTelefono ($telefono){
     $this -> telefono = $telefono;
 }
-public function getTelefono (){
+public function getTelefono(){
     return $this -> telefono;
 }
 public function setContrato ($contrato){
     $this -> contrato = $contrato;
 }
-public function getContrato () {
+public function getContrato() {
     return $this -> contrato;
 }
-
-public function Mostrar () {
-    echo $this -> nombre;
+public function Mostrar() {
+   echo $this -> getNombre();
     echo ", ";
-    echo $this -> password;
+    echo $this -> getPassword();
     echo ", ";
     echo $this -> telefono;
     echo ", ";
     echo $this -> contrato;
     echo "<br>";
+     /*return [$this -> getNombre(), $this -> getPassword(), $this -> telefono, $this -> contrato];*/
 }
-
 }
 ?>
